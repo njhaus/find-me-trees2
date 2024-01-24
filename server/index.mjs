138f368dd706e183fa97ade583725a447fb011cc
njhaus <126741288+njhaus@users.noreply.github.com>
2024-01-24 13:00:48 -0600

    Fix syntax error

--- server/index.mjs
+++ server/index.mjs
@@ -41,7 +41,7 @@ async function main() {
     };
     console.log("Mongoose Connection successful");
   } catch (err) {
-    console.err(err);
+    console.error(err);
   }
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
