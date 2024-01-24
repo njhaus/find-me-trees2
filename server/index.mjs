1fdcb2ad1ca7123bc50972f76950a31d20737390
njhaus <126741288+njhaus@users.noreply.github.com>
2024-01-24 13:43:13 -0600

    Update cors

--- server/index.mjs
+++ server/index.mjs
@@ -54,6 +54,16 @@ const corsOrigin =
     ? "https://find-me-trees-client-production.up.railway.app"
     : "http://localhost:5173";
 
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+
   app.use(
     cors({
       origin: corsOrigin,
@@ -63,14 +73,6 @@ const corsOrigin =
     })
   );
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true}))
