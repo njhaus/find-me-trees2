9c766da83a424dbe2f2acae1e83b10db2dfdfcd6
njhaus <126741288+njhaus@users.noreply.github.com>
2024-01-24 13:38:21 -0600

    Update cors

--- server/index.mjs
+++ server/index.mjs
@@ -63,6 +63,15 @@ const corsOrigin =
     })
   );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true}))
 
