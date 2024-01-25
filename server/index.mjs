6074b4e4d1fa0dd9f13c1da2fc2e1f1bd5801c55
njhaus <126741288+njhaus@users.noreply.github.com>
2024-01-24 20:02:10 -0600

    Just...testing

--- server/index.mjs
+++ server/index.mjs
@@ -1,99 +1,46 @@
-// Imports
-import express from "express";
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import session from "express-session";
-import methodOverride from 'method-override';
-import cors from 'cors';
-
-
-// Auth and security
-import passport from 'passport'
-import LocalStrategy from 'passport-local'
-import crypto from "crypto";
-import dotenv from "dotenv";
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config({ silent: process.env.NODE_ENV === "production" });
 
-// Models
-import User from "./models/user.js";
-
-// Express Setup
 const app = express();
-const port = 3008;
+const port = process.env.PORT;
 
-
-// middleware for allowing react to fetch() from server
-const corsOrigin =
-  process.env.ENVIRONMENT === "prod"
-    ? "https://find-me-trees-client-production.up.railway.app"
-    : "http://localhost:5173";
-
-
-app.options("*", cors());
-
-
-app.use(function (req, res, next) {
-  console.log('this function is doing something')
-  console.log(req);
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://find-me-trees-client-production.up.railway.app"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.header(
-    "Access-Control-Allow-Credentials",
-    true
-  );
-  res.header("Access-Control-Allow-Methods", ["POST", "PUT", "GET", "OPTIONS", "HEAD", "PATCH"]);
-  next();
-});
-
+// const origin = "http://localhost:5173";
+const origin = "https://christmas-list-maker-production.up.railway.app";
+// cors middleware for allowing react to fetch() from server
+var cors = require("cors");
 app.use(
   cors({
-    origin: corsOrigin,
+    origin: origin,
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD", "PATCH"],
     credentials: true,
     preflightContinue: false,
-    optionsSuccessStatus: 204,
   })
 );
 
+// Router
+const router = express.Router();
 
-// TEST ROUTE
-app.get("/", (req, res) => {
-  res.send('"message": "database connection working. / route accessed."' )
-});
+// // Id
+const { v4: uuidv4 } = require("uuid");
 
+// // bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+// // Database
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("christmas_lists.db");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true}))
-
-
-// Body parser -- parse application/json
-app.use(bodyParser.json())
-
-
-// Static setup
-// Import and initialize stuff to make __dirname work
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "client")));
-
-// Cookie setup / cookie parser
-app.use(cookieParser(process.env.SESSION_SECRET));
-
-// setup method override
-app.use(methodOverride("_method"));
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Session setup
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
 const sessionConfig = {
   name: "mr-session",
   secret: process.env.SESSION_SECRET,
@@ -104,104 +51,60 @@ const sessionConfig = {
     httpOnly: true,
     secure: true,
     maxAge: 1000 * 60 * 60 * 24 * 7,
-    sameSite: 'None',
+    sameSite: "None",
   },
 };
 app.use(session(sessionConfig));
 
-// Passport setup
-// Add this before defining routes But AFTER session setup
-app.use(passport.initialize());
-app.use(passport.session());
+// Body parser -- parse application/json
+app.use(bodyParser.json());
 
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// Validation functions
+const validation = require("./middleware/joi_validation.js");
 
+// ++++++++++++++ROUTES+++++++++++++++++++++
 
-// Routes
-import userRoute from './routes/user_routes.js';
-import browseRoute from "./routes/browse_routes.js";
-import loginRoute from "./routes/login_routes.js";
-import treeRoute from "./routes/tree_routes.js"
-import dataRoute from './routes/data_routes.js'
+const homeRoute = require("./routes/home.js");
+const listRoute = require("./routes/list.js");
+const userRoute = require("./routes/user.js");
 
-app.use('/user', userRoute);
-app.use('/browse', browseRoute);
-app.use('/login', loginRoute);
-app.use('/tree', treeRoute);
-app.use('/data', dataRoute);
+app.use("/home", homeRoute);
+app.use("/list", listRoute);
+app.use("/user", userRoute);
 
+// LOGOUT
+app.post("/logout", validation.validateInputs, async (req, res) => {
+  res.clearCookie("list", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    // maxAge: 12 * 60 * 60 * 1000,
+  });
+  res.clearCookie("user", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    // maxAge: 12 * 60 * 60 * 1000,
+  });
+  res.send({ message: "success" });
+});
 
-// Generic route -- for dev purposes only
+// TEST ROUTE
 app.get("/", (req, res) => {
-
-  res.send("Express server is working.");
+  res.send('"message": "database connection working. / route accessed."');
 });
 
-app.post("/", (req, res) => {
-  res.send("Express server is working.");
+// CLOSE DATABASE
+process.on("SIGINT", () => {
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Closed the database connection.");
+    process.exit(0);
+  });
 });
 
-// Error catching routes
-
-app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(403).send(err);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
-
-
-// Mongoose Setup/connection
-
-const mongoConnect = process.env.ENVIRONMENT === 'prod' ? `mongodb+srv://njhaus:${process.env.MONGO_ATLAS}@cluster0.qt7sgci.mongodb.net/?retryWrites=true&w=majority` : "mongodb://127.0.0.1:27017/treesDB";
-
-async function main() {
-  try {
-    await mongoose.connect(mongoConnect), {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedToplology: true,
-      useFindAndModify: false
-    };
-    console.log("Mongoose Connection successful");
-  } catch (err) {
-    console.error(err);
-  }
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
-}
-main();
-
-
-
-// Express connection
-app.listen(port, "0.0.0.0", function () {
-  console.log(`Listening on port ${port}`);
-});
-
-
-
-
-
-// app.use("*", (req, res, next) => {
-//   const error = new AppError("This route does not exist.", 404);
-//   console.log(error);
-//   console.log("MESSAGE:" + error.message);
-//   next(error);
-// });
-
-// app.use((err, req, res, next) => {
-//   console.log(err);
-//   const { message = "Sorry, something went wrong.", status = 500 } = err;
-//   console.log(message, status);
-//   res.status(status);
-//   res.render("error/error.ejs", { status, message });
-// });
-
-
-
-// // Crypto -- get random bytes
-
-// crypto.randomBytes(64, (err, buf) => {
-//   if (err) throw err;
-//   console.log(`${buf.length} bytes of random data: ${buf.toString('hex')}`);
-// });
